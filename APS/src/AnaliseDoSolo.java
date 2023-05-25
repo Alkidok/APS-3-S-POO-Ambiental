be5d593b207b41cc2e2e2693394881c7fd8901c8
Vitor Gabriel Bezerra De Siqueira <67169422+Alkidok@users.noreply.github.com>
2023-05-25 00:01:40 -0300

    Update AnaliseDoSolo.java
    valores de elementos

--- APS/src/AnaliseDoSolo.java
+++ APS/src/AnaliseDoSolo.java
@@ -52,36 +52,36 @@ public class AnaliseDoSolo {
         resultado.append("\n");
 
         resultado.append("PH DO SOLO: " + this.phSolo + "\n");
-        if (Double.parseDouble(this.phSolo) < 1) {
+        if (Double.parseDouble(this.phSolo) < 6) {
             resultado.append("- O pH do solo está abaixo do recomendado, corrigir com calcário.\n");
         }
         resultado.append("\n");
 
         resultado.append("FÓSFORO NO SOLO: " + this.fosforoSolo + " mg/dm³\n");
-        if (Double.parseDouble(this.fosforoSolo) < 1) {
+        if (Double.parseDouble(this.fosforoSolo) < 15) {
             resultado.append("- O teor de fósforo no solo está abaixo do recomendado, corrigir com adubo fosfatado.\n");
         }
         resultado.append("\n");
 
         resultado.append("POTÁSSIO NO SOLO: " + this.potassioSolo + " cmolc/dm³\n");
-        if (Double.parseDouble(this.potassioSolo) < 1) {
+        if (Double.parseDouble(this.potassioSolo) < 150) {
             resultado.append("- O teor de potássio no solo está abaixo do recomendado, corrigir com adubo potássico.\n");
         }
         resultado.append("\n");
 
         resultado.append("CÁLCIO NO SOLO: " + this.calcioSolo + " cmolc/dm³\n");
-        if (Double.parseDouble(this.calcioSolo) < 1) {
+        if (Double.parseDouble(this.calcioSolo) < 1500) {
             resultado.append("- O teor de cálcio no Solo está abaixo do recomendado, corrigir com calcário.\n");
         }
         resultado.append("\n");
         resultado.append("MAGNÉSIO NO SOLO: " + this.magnesioSolo + " cmolc/dm³\n");
-        if (Double.parseDouble(this.magnesioSolo) < 1) {
+        if (Double.parseDouble(this.magnesioSolo) < 200) {
             resultado.append("- O teor de magnésio no solo está abaixo do recomendado, corrigir com sulfato de magnésio.\n");
         }
         resultado.append("\n");
     
         resultado.append("ENXOFRE NO SOLO: " + this.enxofreSolo + " mg/dm³\n");
-        if (Double.parseDouble(this.enxofreSolo) < 1) {
+        if (Double.parseDouble(this.enxofreSolo) < 15) {
             resultado.append("- O teor de enxofre no solo está abaixo do recomendado, corrigir com gesso agrícola.\n");
         }
         resultado.append("\n");
@@ -93,7 +93,7 @@ public class AnaliseDoSolo {
         resultado.append("\n");
     
         resultado.append("MATÉRIA ORGÂNICA NO SOLO: " + this.materiaOrganicaSolo + " g/dm³\n");
-        if (Double.parseDouble(this.materiaOrganicaSolo) < 1) {
+        if (Double.parseDouble(this.materiaOrganicaSolo) < 30) {
             resultado.append("- O teor de matéria orgânica no solo está abaixo do recomendado, correção com adição de matéria orgânica.\n");
         }
         return resultado.toString();
